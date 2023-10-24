7986a7c9b8a17ac4ff3b7bb5abf5b0ffcdb24742
changhyun9 <ckdgus1713@naver.com>
2023-10-24 13:36:04 +0900

    removed cached

--- wise-saying/src/main/java/org/example/Main.java
+++ wise-saying/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("== 명언 앱 ==");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+
+        while (!input.equals("종료")) {
+            System.out.print("명령) ");
+            input = bf.readLine();
+
+        }
     }
 }
